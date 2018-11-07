38a9722bc15f73068108e82cd6bd85500e93e488
Kyle Mathews <mathews.kyle@gmail.com>
2018-11-06 21:12:14 -0800

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,11 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-128882377-1",
+      },
+    },
   ],
 };
